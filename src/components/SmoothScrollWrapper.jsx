8f1a16d83d08dd74e072b0a5db5a6be849f31ff9
ishu303 <ishupatel9454@gmail.com>
2025-12-07 17:13:28 +0530

    4 servey code commit deployment frendaly

--- src/components/SmoothScrollWrapper.jsx
+++ src/components/SmoothScrollWrapper.jsx
@@ -4,12 +4,12 @@ import Lenis from '@studio-freight/lenis';
 export default function SmoothScrollWrapper({ children }) {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 1,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            duration: 0.6,
+            easing: (t) => 1 - Math.pow(1 - t, 3),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
             smoothWheel: true,
-            wheelMultiplier: 0.8,
+            wheelMultiplier: 1.2,
             smoothTouch: false,
             touchMultiplier: 2,
             infinite: false,
